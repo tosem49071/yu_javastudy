d59a4524f6e3dc5ee357fe7d9926d2e582ebe2c8
SIST <SIST@DESKTOP-CKK1LCA>
2023-03-31 08:59:35 +0900

    1111

---
+++ Chapter2_변수와연산자/src/MainClass.java
@@ -0,0 +1,15 @@
+/*
+ *    1. 기본형 : 메모리 크기를 지원한다
+ *              1byte / 2byte / 4byte / 8byte => 자바에서 지원
+ *              => 12byte (2+2+8)
+ *              4+4+4 => 배열
+ *              2+4+8+1 => 클래스
+ */
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
