df877f67bec5b6e2c99df6349e2782d97ed718eb
SIST <SIST@DESKTOP-CKK1LCA>
2023-04-03 17:53:00 +0900

    0000

---
+++ Chapter3_제어문_선택문_1/src/선택문_2.java
@@ -0,0 +1,44 @@
+// 성적 계산
+/*
+ *   case 3: case 4: case 5: ==> if(month>=3 && month<=5)
+ *      System.out.println("봄")
+ *      break
+ */
+import java.util.Scanner;
+public class 선택문_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan=new Scanner(System.in);
+		System.out.println("세개의 정수 입력(10 20 30):");
+		int kor=scan.nextInt();
+		int eng=scan.nextInt();
+		int math=scan.nextInt();
+		int avg=(kor+eng+math)/3;
+		//case는 중복할 수 없다
+		
+		// 학점 출력
+		char score='A';
+		switch(avg/10)
+		{
+		case 10://100
+		case 9://99~90 => (avg/10)==10 || (avg/10)==9
+			score='A';
+			break;
+		case 8:
+			score='B';
+			break;
+		case 7:
+			score='C';
+			break;
+		case 6:
+			score='D';
+			break;
+		default:// 생략이 가능 ==> else
+			score='F';
+		}
+		System.out.println("학점:"+score);
+
+	}
+
+}
