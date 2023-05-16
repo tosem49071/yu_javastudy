440e47a9ea52e86f58671ef57d873f0f34a2f9d9
SIST <SIST@DESKTOP-CKK1LCA>
2023-05-16 18:00:28 +0900

    0000

--- 2주차정리/src/제어문_정리.java
+++ 2주차정리/src/제어문_정리.java
@@ -59,7 +59,18 @@ public class 제어문_정리 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		class X {
+		      X() { System.out.print(1);  }
+		      X(int x) { this();  System.out.print(2); }
+		}
+
+		public class Y extends X {
+		      Y() { super(6);  System.out.print(3); }
+		      Y(int y) { this();  System.out.println(4); }
+		      public static void main(String[] args) {
+		          new Y(5);
+		      }
+		}
 	}
 
 }
